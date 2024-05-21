23f485b2c546235581aeb550e39f255ffd720ea3
tpring <pororo1@naver.com>
2024-05-21 16:17:22 +0900

    ex4 Solced

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import { useState } from "react";
-import FilterButtons from "./components/FilterButtons";
-import StudentList from "./components/StudentList";
+import { useState } from 'react';
+import FilterButtons from './components/FilterButtons';
+import StudentList from './components/StudentList';
 
 function App() {
-  const initialStudents = [
-    { name: "Alice", age: 17, grade: "A" },
-    { name: "Bob", age: 18, grade: "B" },
-    { name: "Charlie", age: 16, grade: "C" },
-    { name: "Diana", age: 19, grade: "D" },
-  ];
+    const initialStudents = [
+        { name: 'Alice', age: 17, grade: 'A' },
+        { name: 'Bob', age: 18, grade: 'B' },
+        { name: 'Charlie', age: 16, grade: 'C' },
+        { name: 'Diana', age: 19, grade: 'D' },
+    ];
 
-  const [filteredStudents, setFilteredStudents] = useState(initialStudents);
+    const [filteredStudents, setFilteredStudents] = useState(initialStudents);
 
-  return (
-    <div>
-      <h1>학생 목록</h1>
-      {/* TODO: FilterButtons 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
-      <FilterButtons
-      /* 필요한 props를 여기에 전달하세요. */
-      />
+    return (
+        <div>
+            <h1>학생 목록</h1>
+            {/* TODO: FilterButtons 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
+            <FilterButtons
+                /* 필요한 props를 여기에 전달하세요. */
+                setFilteredStudents={setFilteredStudents}
+                initialStudents={initialStudents}
+            />
 
-      {/* TODO: StudentList 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
-      <StudentList
-      /* 필요한 props를 여기에 전달하세요. */
-      />
-    </div>
-  );
+            {/* TODO: StudentList 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
+            <StudentList
+                /* 필요한 props를 여기에 전달하세요. */
+                filteredStudents={filteredStudents}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,11 +1,16 @@
 // TODO: StudentList 컴포넌트를 작성하세요. props로 학생 목록을 받아와서 표시해야 합니다.
-function StudentList(/* 필요한 props를 여기에 전달하세요 */ {}) {
-  return (
-    <ul>
-      {/* 여기에 학생 목록을 표시하는 로직을 작성하세요. */}
-      {/* ex: 홍길동 (20세) - A등급 */}
-    </ul>
-  );
+function StudentList(/* 필요한 props를 여기에 전달하세요 */ { filteredStudents }) {
+    return (
+        <ul>
+            {filteredStudents.map((student) => (
+                <div key={student.name}>
+                    <li>{student.name}</li>
+                    나이 : {student.age}
+                    학점 : {student.grade}
+                </div>
+            ))}
+        </ul>
+    );
 }
 
 export default StudentList;
