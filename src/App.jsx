fec2e0e7c03b8b9a43f44b61a8d7560d968bc76a
tpring <pororo1@naver.com>
2024-05-21 12:18:04 +0900

    Solve problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 export default function App() {
-  const students = [
-    { name: "Alice", age: 17, grade: "A" },
-    { name: "Bob", age: 18, grade: "B" },
-    { name: "Charlie", age: 16, grade: "C" },
-    { name: "Diana", age: 19, grade: "D" },
-  ];
+    const students = [
+        { name: 'Alice', age: 17, grade: 'A' },
+        { name: 'Bob', age: 18, grade: 'B' },
+        { name: 'Charlie', age: 16, grade: 'C' },
+        { name: 'Diana', age: 19, grade: 'D' },
+    ];
 
-  // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+    // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
+    const filteredStudents = students.filter((students) => students.age >= 18);
 
-  return (
-    <div>
-      <h1>학생 목록</h1>
-      <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-        {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
-      </ul>
-    </div>
-  );
+    const displayInfo = (students) => {
+        alert(`나이: ${students.age}, 점수: ${students.grade}`);
+    };
+
+    return (
+        <div>
+            <h1>학생 목록</h1>
+            <ul>
+                {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+                {filteredStudents.map((students) => (
+                    /* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/
+                    <li key={students.name}>
+                        <button onClick={() => displayInfo(students)}> {students.name} </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
